ecf65d992066104d02f6e0dfb2eae7f58f647639
sesto104 <izgmadminno1_hhu>
2019-04-07 16:56:15 +0200

    worked on my homework

---
+++ blatt0.py
@@ -0,0 +1,86 @@
+import math
+import scipy.special
+
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+
+def flatten(n):
+    result = []
+    if isinstance(n, list):
+        for i in n:
+            result = result + i
+        return result
+    return n
+
+
+def print_pascal(depth):
+    result = []
+    for i in range(depth):
+        result = result + [print_pascal_it(i)]
+    return result
+
+def print_pascal_it(depth):
+    result = []
+    for i in range(depth+1):
+        result = result + [scipy.special.binom(depth, i)]
+    return result
+
+
+def solve_equation(a, b, c):
+    if a > 1:
+        return solve_equation(a / a, b / a, c / a)
+    if a == 0:
+        raise ValueError("Not a quadratic equation")
+    if a < 0:
+        return solve_equation(a / a, b / a, c / a)
+    first = - b / 2
+    second = ((b / 2) ** 2) - c
+    if second < 0:
+        raise ValueError("This Equations has no solution in the real numbers")
+    second_mod = math.sqrt(second)
+    return [first + second_mod, first - second_mod]
+
+
+def fizz_buzz(n):
+    result = []
+    for i in range(n):
+        if i == 0:
+            continue
+        if i % 3 == 0 and i % 5 == 0:
+            result = result + ["Fizz Buzz"]
+            continue
+        if i % 3 == 0:
+            result = result + ["Fizz"]
+            continue
+        if i % 5 == 0:
+            result = result + ["Buzz"]
+            continue
+        result = result + [i]
+    return result
+
+
+def myint(bin):
+    temp = bin[::-1]
+    leng = len(bin)
+    result = 0
+    multiplicator = 1
+    for i in range(0, leng - 2):
+        result = result + int(temp[i]) * multiplicator
+        multiplicator = multiplicator * 2
+    return result
+
+
+def mybin(inte):
+    if inte == 0:
+        return "0b0"
+    input = ""
+    rechnung = inte
+    while rechnung > 0:
+        help = int(rechnung / 2)
+        temp = rechnung - help * 2
+        input = input + str(temp)
+        rechnung = help
+        print(temp)
+    return "0b" + input[::-1]
--- test_blatt0.py
+++ test_blatt0.py
@@ -1,30 +1,67 @@
-#from blatt0 import pascal
-#from blatt0 import flatten
-#from blatt0 import fizz_buzz
-#from blatt0 import solve_equation
-#from blatt0 import myint, mybin
+from blatt0 import print_pascal
+from blatt0 import fizz_buzz
+from blatt0 import solve_equation
+from blatt0 import myint, mybin
+from blatt0 import is_palindrome
+from blatt0 import flatten
 import random
 
-def test_pascal():
-    pass # TODO
 
-def test_flatten():
-    pass # TODO
+def test_palindrom():
+    input = "anna"
+    assert is_palindrome(input) == True
+
+
+def test_pascal():
+    input = 4
+    assert print_pascal(5) == [[1.0], [1.0, 1.0], [1.0, 2.0, 1.0], [1.0, 3.0, 3.0, 1.0], [1.0, 4.0, 6.0, 4.0, 1.0]]
+
+
+def test_flatten1():
+    input = []
+    assert flatten(input) == []
+
+
+def test_flatten2():
+    input = [["Hallo"]]
+    assert flatten(input) == ["Hallo"]
+
+
+def test_flatten3():
+    input = [["Hallo"]["Welt"]]
+    assert flatten(input) == ["Hallo" "Welt"]
+
+
+def test_solve_equation1():
+    assert solve_equation(1, 0, 0) == [0.0, 0.0]
+
+
+def test_solve_equation3():
+    assert solve_equation(1, -6, 5) == [5.0, 1.0]
+
+
+def test_solve_equation4():
+    assert solve_equation(2, -3, 1) == [2.0, 0.0]
+
 
 def test_fizz_buzz():
-    pass # TODO
+    output = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "Fizz Buzz", 16, 17, "Fizz",
+              19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "Fizz Buzz", 31, 32, "Fizz", 34, "Buzz",
+              "Fizz"]
+    assert fizz_buzz(37) == output
+
 
 def test_solve_equation():
-    pass # TODO
+    pass  # TODO
+
 
 def test_int_to_bin():
     input = random.sample(range(1000), 10)
     for x in input:
         assert mybin(x) == bin(x)
 
+
 def test_bin_to_int():
     input = [(i, bin(i)) for i in random.sample(range(1000), 10)]
     for i, bini in input:
         assert myint(bini) == i
-
-# TODO: more tests
